00b7b9f6ff6a7e98c1336a2830c47a8797a3468e
Mohd Aftab <ghub02363@gmail.com>
2025-11-15 19:00:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let notesContainer= document.querySelector(".notes-container");
+let createbtn= document.querySelector(".btn");
+let notes= document.querySelector(".input-box");
+
+
+// Store notes in Local Storage
+
+
+
+createbtn.addEventListener("click", function(){
+    // create P tag
+    let inputBox= document.createElement("p");
+    inputBox.className="input-box";
+    inputBox.setAttribute("contenteditable", "true");
+
+    // create img tag 
+    let img= document.createElement("img");
+    img.src= "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+
+})
+
+// To remove the note on clicking the delete icon
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+    }
+})
